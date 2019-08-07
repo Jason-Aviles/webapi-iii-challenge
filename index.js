c7d2debced71c4d91e2a3f619904239ed09ddf73
Jason Aviles <sandman2k18@outlook.com>
2019-08-07 16:31:51 -0400

    created the logger middleware and used it globally

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // code away!
-const express = require('express');
+const express = require("express");
 
 const server = express();
-const port = 5000
+const port = 5000;
 
-server.use(express.json())
+function logger(req, res, next) {
+  console.log(`
+  [${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+    "Origin"
+  )} `);next()
+}
 
+server.use(express.json());
 
+server.use(logger)
 
-
-
-server.listen(port, ( )=> console.log('connected'))
-
+server.listen(port, () => console.log("connected"));
