5ae8cebb024a43360bfd0725fc7621160d1325e7
Jason Aviles <sandman2k18@outlook.com>
2019-08-08 14:45:26 -0400

    made my server and logger middleware

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // code away!
 const express = require("express");
-
+const 
 const server = express();
 const port = 5000;
 
 function logger(req, res, next) {
   console.log(`
   [${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
-    "Origin"
+    "Origin "
   )} `);next()
 }
-
+var 
 server.use(express.json());
 
 server.use(logger)
