fedef42024705768e105a804cce06c2b4c3d59ee
Jason Aviles <sandman2k18@outlook.com>
2019-08-08 14:46:24 -0400

    finnshed my 3 middlewares and 60% routes done

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,25 +1,34 @@
 const express = 'express';
 
 const router = express.Router();
+const DBuser = require('./userDb')
+
 
 router.post('/', (req, res) => {
-
+  const body = req.body;
+  DBuser.insert(body).then(user => res.status(201).json(user))
 });
 
 router.post('/:id/posts', (req, res) => {
+  const body = req.body;
+
+  DBuser.insert(body).then(user => res.status(201).json(user))
 
 });
 
 router.get('/', (req, res) => {
-
+DBuser.get().then(user => res.status(200).json(user))
 });
 
 router.get('/:id', (req, res) => {
+const {id} = req.params
+DBuser.getById(id).then(user => res.status(200).json(user))
 
 });
 
 router.get('/:id/posts', (req, res) => {
-
+  const {id} = req.params
+  DBuser.getUserPosts(id).then(user => res.status(200).json(user))
 });
 
 router.delete('/:id', (req, res) => {
@@ -33,15 +42,37 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
+const {id} = req.params;
+if(!id){
+  res.status(400).json({ message: "invalid user id" })
+}
+else{next()}
 
 };
 
 function validateUser(req, res, next) {
-
-};
+  const body = req.body;
+  const name = req.body.name;
+  if (!body) {
+    res.status(400).json({ error: "missing user data" });
+  } else if (!name) {
+    res.status(400).json({ error: "missing required name field" });
+  } else {
+    next();
+  }
+}
 
 function validatePost(req, res, next) {
-
+  const body = req.body;
+  const text = req.body;
+  if (!body) {
+    res.status(400).json({message: "missing post data" });
+  } else if (!text) {
+    res.status(400).json({  message: "missing required text field" });
+  } else {
+    next();
+  }
 };
 
 module.exports = router;
+  
