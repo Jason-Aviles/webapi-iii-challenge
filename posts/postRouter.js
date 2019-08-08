d6540bbd62fe76d28459fcf32d4904eb5bee3ecc
Jason Aviles <sandman2k18@outlook.com>
2019-08-08 14:57:44 -0400

    finnshed my routes

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,27 @@
 const express = 'express';
 
 const router = express.Router();
-
+const DBpost = require('./postDb')
 router.get('/', (req, res) => {
-
+  DBpost.get().then(user => res.status(200).json(user)).catch(err => res.status(500).json({message:'check your backend'}))
 });
 
 router.get('/:id', (req, res) => {
-
+  const {id} = req.params
+  DBpost.getById(id).then(user => res.status(200).json(user)).catch(err => res.status(500).json({message:'check your backend'}))
+  
 });
 
 router.delete('/:id', (req, res) => {
-
+  const {id} = req.params
+  DBpost.remove(id).then(user => res.status(203).json(user)).catch(err => res.status(500).json({message:'check your backend'}))
 });
 
 router.put('/:id', (req, res) => {
+  const {id} = req.params
+  const body = req.body;
 
+  DBpost.update(id,body).then(user => res.status(201).json(user)).catch(err => res.status(500).json({message:'check your backend'}))
 });
 
 // custom middleware
