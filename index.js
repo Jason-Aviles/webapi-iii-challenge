c50ff479d234759d2a956e6d0e842aec23741326
Jason Aviles <sandman2k18@outlook.com>
2019-08-08 16:22:34 -0400

    done

--- index.js
+++ index.js
@@ -21,4 +21,19 @@ server.use(logger)
 server.use('/post',post)
 server.use('/users',user)
 
+
+
+server.get('/', async (req, res) => {
+  try {
+    const shoutouts = await db('shoutouts');
+    const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+  }
+});
+
+
+
 server.listen(port, () => console.log("connected"));
